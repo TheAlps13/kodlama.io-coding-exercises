bf8cbf294c0b43524b31f9ecb8b9f198650352f6
Alper <thealps13@gmail.com>
2022-08-23 11:52:26 +0300

    Kodlama.io Coding Exercises 1 | Added ClassMethodDemo exercise

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        private List<Customer> _customers = new List<Customer>();
+
+        public void AddCustomer(Customer customer)
+        {
+            _customers.Add(customer);
+            Console.WriteLine($"Customer {customer.Name} added\n");
+        }
+
+        public void RemoveCustomer(Customer customer)
+        {
+            if(_customers.Remove(customer))
+                Console.WriteLine($"Customer {customer.Name} has been succesfully deleted\n");
+            else
+                Console.WriteLine($"Customer {customer.Name} could not be deleted\n");
+        }
+
+        public void ListCustomers()
+        {
+            Console.WriteLine("List of customers; \n ");
+            foreach (Customer customer in _customers)
+            {
+                Console.WriteLine("####################################");
+
+                Console.WriteLine(
+                    $"Id: {customer.Id}\n" +
+                    $"Name: {customer.Name}\n" +
+                    $"Last Name: {customer.LastName}");
+
+
+                Console.WriteLine("####################################\n");
+            }
+               
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager();
+
+            Customer customer1 = new Customer
+            {
+                Id = 1,
+                Name = "Alper",
+                LastName = "Tasci"
+            };
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                Name = "Kemal",
+                LastName = "Johnson"
+            };
+
+            Customer customer3 = new Customer
+            {
+                Id = 3,
+                Name = "Goldy",
+                LastName = "King"
+            };
+
+            ///////////////////////////////////////////////////////////////
+
+            customerManager.AddCustomer(customer1);
+            customerManager.AddCustomer(customer2);
+            customerManager.AddCustomer(customer3);
+
+            customerManager.ListCustomers();
+
+            customerManager.RemoveCustomer(customer2);
+
+            customerManager.ListCustomers();
+        }
+    }
+}
