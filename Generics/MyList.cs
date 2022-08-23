141b79b5e9e3ae78eee1c8320a8fc34e9fb0bea2
Alper <thealps13@gmail.com>
2022-08-23 21:38:38 +0300

    Kodlama.io Coding Exercises 3 | Added Generics exercise

---
+++ Generics/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class MyList<T>
+    {
+        private T[] _items;
+        public int Count { get; private set; }
+
+        public MyList()
+        {
+            _items = new T[0];
+        }
+        public void Add(T item)
+        {
+            Count++;
+            Array.Resize(ref _items, Count);
+            _items[Count - 1] = item;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return _items[index];
+            }
+            set
+            {
+                _items[index] = value;
+            }
+        }
+
+    }
+
+
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> list = new MyList<string>();
+
+            list.Add("Hey");
+            list.Add("Hello");
+            list.Add("Stop");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+          
+                
+        }
+    }
+}
