b55159734c6ac9df1385d7cc2cad4abb4c93a903
Alper <alper.tasci@hotmail.com>
2022-08-25 21:18:14 +0300

    Kodlama.io exercises 4 | Added Generics-2 example

---
+++ Generics-2/MyDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_2
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        private TKey[] _keys;
+        private TValue[] _values;
+
+        public int Count { get; private set; }
+
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            Count++;
+
+            Array.Resize(ref _keys, Count);
+            Array.Resize(ref _values, Count);
+
+            _keys[Count - 1] = key;
+            _values[Count - 1] = value;
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                int index;
+                if ((index = Array.IndexOf(_keys, key)) != -1)
+                {
+                     TValue value = _values[index];
+                return value;
+                }
+
+                throw new Exception("Key not found");
+            }
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine($"Key: {_keys[i]}" +
+                    $"\nValue: {_values[i]}" +
+                    "\n***************");
+            }
+        }
+    }
+}
---
+++ Generics-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+
+            dictionary.Add("Alper", "Tasci");
+            dictionary.Add("Kevin", "Flyer");
+            dictionary.Add("Michael", "Jakesun");
+
+
+            dictionary.List();
+
+            // This will search for the key and  return related value if exists
+            Console.WriteLine($"Value of Alper: {dictionary["Alper"]}");
+            Console.WriteLine($"Value of Kevin: {dictionary["Kevin"]}");
+        }
+    }
+}
